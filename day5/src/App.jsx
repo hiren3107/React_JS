092e5c47382058acaea53a76a7a4618acc3d341b
hiren3107 <agravathiren07@gmail.com>
2024-11-25 12:39:46 +0530

    day5

--- day5/src/App.jsx
+++ day5/src/App.jsx
@@ -1,21 +1,58 @@
 import { useState } from 'react';
 import './App.css';
-import login from "./loginimage.png"
+
+
 
 function App() {
-  var [h,seth] = useState("show")
+  var [h,seth] = useState("hide")
+
+  function Log()
+{  
+  return <div className="App">
+       <div className='main'>
+        <div className='img'>
+          <div className='logA'>
+      <h1 className='h1'>Login</h1>
+      <p className='p'>Get access to your Orders,<br /> Wishlist and Recommendations</p>
+      <img src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/login_img_c4a81e.png" className='v' alt="" />
+     </div>
+        </div>
+        <div className='w-[510px] pt-[56px] ps-[35px] pe-[35px]'>
+          <input className='in' placeholder='Enter Email / Mobile number' type="text" />
+          <p className='text-[12px] text-[#878787] font-[500] mb-[20px]'>By continuing, you agree to Flipkart's <a href="" className='text-[#2878F1]'> Terms of Use </a> and <a href="" className='text-[#2878F1]'> Privacy Policy.</a></p>
+          <button className='bb mb-[270px]'>Request OTP</button>
+          <div className='text-center'>
+            <a href="" className='text-[#2878F1] font-[500] text-[14px]' onClick={() => seth("hide")}>New to Flipkart? Create an account</a>
+          </div>
+        </div>
+      </div> 
+    </div>
+}
+
+function Sin(){
+  return <div className="Appp">
+    <div className='main'>
+     <div className='img'>
+     <div className='signA'>
+    <h1 className='h1'>Looks like you're new here!</h1>
+      <p className='p'>Sign up with your mobile number to get started</p>
+      <img src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/login_img_c4a81e.png" className='v1' alt="" />
+    </div>
+     </div>
+     <div className='w-[510px] pt-[56px] ps-[35px] pe-[35px]'>
+       <input className='in' placeholder='Enter Mobile number' type="text" />
+       <p className='text-[12px] text-[#878787] font-[500] mb-[20px]'>By continuing, you agree to Flipkart's <a href="" className='text-[#2878F1]'> Terms of Use </a> and <a href="" className='text-[#2878F1]'> Privacy Policy.</a></p>
+       <button className='bb mb-[20px]'>Continue</button>
+       <button className='bb2 mb-[20px]' onClick={() => seth("show")} >Existing User? Log in</button>
+     </div>
+   </div> 
+ </div>
+}
  
   return (
-    <div className="App">
-      <div className='main'>
-        <div className='img'>
-          <img className='l' src={login} alt=""/>
-        </div>
-        <div className='lakhan'>
-          
-        </div>
-      </div>
-    </div>
+    <div>
+      { h == "show" ? <Log/> : <Sin/> }
+    </div>    
   );
 }
 
--- day5/src/index.js
+++ day5/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './header';
+import Footer from './footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <div>
+      <Header/>
+      <App /> 
+      <Footer/>
+    </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
